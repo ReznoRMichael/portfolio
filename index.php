2a1802aa5f05be8eb217eaa33b338a889dbc7f42
ReznoRMichael <mic3arena@gmail.com>
2019-06-12 17:36:04 +0200

    made the index.php and header, footer .html files

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include_once('header.html'); ?>
+
+<!-- Container: Main Content -->
+
+<div class="container-fluid">
+    <div id="generate" class="content content-center">
+        
+        <div class="michaelreznor">
+
+            <h2>Hi! My name is Michał Stefaniak.</h2>
+
+            <p>An enthusiastic, self-educated former Lead Graphic Designer currently offering his humble skills as a helping hand to an experienced mentor who would take me under his/her wing - for free or on a paid intern.</p>
+
+            <p>In exchange I can offer you many varied skills and experience - my life forced me to self-education in PCs, IT support, graphic design, photography, games and writing guides about technology, audio, hardware, software. In my previous job I had many different roles at once from which my experience could become useful (all are mentioned in <a href="https://www.linkedin.com/in/michal-stefaniak/" target="_blank">my LinkedIn profile</a>). Since November 2018 I'm learning programming to pursue my strongest childhood dream that previously wasn't possible for me to achieve.</p>
+
+            <p>If you need someone who always enjoys a challenge, solving logical problems, helping other people, loves constant self-improvement, reading, writing and learning new things + has attention to details - I'm that person.</p>
+
+            <p>if(place to sit && task to work on) I will be totally happy to help you and honoured to learn new things from the best :) else never stop trying.</p>
+
+            <p>My end goal and dream since childhood? To be a programmer - preferably to work in a team of great people on back-end and multiplatform apps that could improve many people's lives. If I learn hard enough, I believe that this dream could one day become reality.</p>
+
+            <div class="orangebutton" onclick="ninjaSpoiler(0)" id="ninja0">Open Tech Spoiler ▼</div>
+            <div class="ninjahiddenspoiler" id="ninjaClick0">
+                <p><strong>This page:</strong><br>
+                - PHP, HTML5, CSS, JavaScript, JSON, jQuery, BootStrap 4<br>
+                - One-Page Design with header.html and footer.html included with PHP<br>
+                - dynamic generation of titles, descriptions, thumbnails, links from one JSON file through a single function<br>
+                - two-dimensional array in JSON file<br>
+                - bLazy to lazy-load image files<br>
+                - file-size web optimized .png and .jpg images (pingo, Irfanview)
+                </p>
+
+                <p>Have fun! :)</p>
+            </div>
+
+        </div>
+        
+    </div> <!-- end content -->
+</div> <!-- end container-fluid -->
+
+<!-- Footer -->
+
+<?php include_once('footer.html'); ?>
